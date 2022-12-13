dce52d0bedfcf956fa01ce947698db63067df742
JaiShankar <jaishankar.a1603@gmail.com>
2022-12-13 05:54:41 +0530

    added navbar

---
+++ index.js
@@ -0,0 +1,24 @@
+const navMenu = document.querySelector(".nav-Menu");
+function hideNav(){
+    if (window.innerWidth <= 768){
+        navMenu.style.display = "none";
+    }
+    else{
+        navMenu.style.display = "block";
+    }
+}
+window.addEventListener("resize",hideNav);
+window.addEventListener("load",hideNav);
+
+const burgerBox = document.querySelector(".burger-box");
+let menuOpen = flase;
+window.addEventListener("click",() => {
+    if(!menuOpen){
+        burgerBox.classList.add("open");
+        menuOpen = true;
+    }
+    else{
+        burgerBox.classList.remove("open");
+        menuOpen = false;
+    }
+})
